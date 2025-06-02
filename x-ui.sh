32181df3d062d52a2f31282f5981be9a3bc23fb8
875706361 <your-email@example.com>
2025-06-02 20:00:58 +0800

    调整脚本链接指向个人仓库，保留软件包下载链接指向3x-ui

--- x-ui.sh
+++ x-ui.sh
@@ -315,7 +315,7 @@ install_bbr() {
 }
 
 update_shell() {
-    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/MHSanaei/3x-ui/raw/main/x-ui.sh
+    wget -O /usr/bin/x-ui -N --no-check-certificate https://github.com/875706361/x-ui_he/raw/master/x-ui.sh
     if [[ $? != 0 ]]; then
         echo ""
         LOGE "下载脚本失败，请检查本机能否连接 Github"
